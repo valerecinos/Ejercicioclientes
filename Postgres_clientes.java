ee41a82c2693d3b2f15761e97bbb641fd2eb817e
valerecinos <124801457+valerecinos@users.noreply.github.com>
2023-08-29 22:07:20 -0600

    Add files via upload

---
+++ Postgres_clientes.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.postgres_clientes;
+
+/**
+ *
+ * @author 
+ */
+public class Postgres_clientes {
+
+    public static void main(String[] args) {
+        ViewClientes objetoVista=new ViewClientes();
+        objetoVista.setVisible(true);
+    }
+}
+
